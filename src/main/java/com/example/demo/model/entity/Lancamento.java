b6f5bc13d2a34d0382fd37b05e498b8fe882170c
George Salu <george.salu10@gmail.com>
2020-02-18 13:26:53 -0300

    incluindo o lombok

--- src/main/java/com/example/demo/model/entity/Lancamento.java
+++ src/main/java/com/example/demo/model/entity/Lancamento.java
@@ -20,36 +20,36 @@ import org.springframework.data.convert.Jsr310Converters;
 @Entity
 @Table(name = "lancamento", schema = "financas")
 public class Lancamento {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private Long id;
-	
+
 	@Column(name = "descricao")
 	private String descricao;
 
 	@Column(name = "mes")
 	private Integer mes;
-	
+
 	@Column(name = "ano")
 	private Integer ano;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "id_usuario")
 	private Usuario usuario;
-	
+
 	@Column(name = "valor")
 	private BigDecimal valor;
-	
+
 	@Column(name = "data_cadastro")
 	@Convert(converter = Jsr310Converters.DateToLocalDateConverter.class)
 	private LocalDate dataCadastro;
-	
+
 	@Column(name = "tipo")
 	@Enumerated(value = EnumType.STRING)
 	private TipoLancamento tipo;
-	
+
 	@Column(name = "status")
 	@Enumerated(value = EnumType.STRING)
 	private StatusLancamento status;
@@ -199,6 +199,5 @@ public class Lancamento {
 				+ usuario + ", valor=" + valor + ", dataCadastro=" + dataCadastro + ", tipo=" + tipo + ", status="
 				+ status + "]";
 	}
-	
-	
+
 }
