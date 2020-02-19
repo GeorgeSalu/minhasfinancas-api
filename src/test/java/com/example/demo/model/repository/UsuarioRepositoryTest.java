c0b6a8efa9e71bfc3528355945ac55f1399a0138
George Salu <george.salu10@gmail.com>
2020-02-19 14:34:28 -0300

    melhorando nossos teste de integracao com @DataJpaTest

--- src/test/java/com/example/demo/model/repository/UsuarioRepositoryTest.java
+++ src/test/java/com/example/demo/model/repository/UsuarioRepositoryTest.java
@@ -4,12 +4,16 @@ import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.example.demo.model.entity.Usuario;
 
-@SpringBootTest
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
 @RunWith(SpringRunner.class)
 public class UsuarioRepositoryTest {
   
