06b666f28c51cd5c85721329a932f7aa8ad2e9e7
George Salu <george.salu10@gmail.com>
2020-02-19 14:20:32 -0300

    implementacao do metodo autentica usuario

--- src/main/java/com/example/demo/service/impl/UsuarioServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UsuarioServiceImpl.java
@@ -1,9 +1,13 @@
 package com.example.demo.service.impl;
 
+import com.example.demo.exception.ErroAutenticacao;
 import com.example.demo.exception.RegraNegocioException;
 import com.example.demo.model.entity.Usuario;
 import com.example.demo.model.repository.UsuarioRepository;
 import com.example.demo.service.UsuarioService;
+
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,8 +22,14 @@ public class UsuarioServiceImpl implements UsuarioService{
 
   @Override
   public Usuario autenticar(String email, String senha) {
-    // TODO Auto-generated method stub
-    return null;
+    Optional<Usuario> usuario = repository.findByEmail(email);
+    if(!usuario.isPresent()) {
+      throw new ErroAutenticacao("Usuario nao encontrado para o email informado");
+    }
+    if(!usuario.get().getSenha().equals(senha)) {
+      throw new ErroAutenticacao("senha invalida");
+    }
+    return usuario.get();
   }
 
   @Override
