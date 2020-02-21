eee12e80a826bc7c72e772320495fc3ee0762618
George Salu <george.salu10@gmail.com>
2020-02-21 11:26:14 -0300

    implementando o endpoint de autenticar usuarios

--- src/main/java/com/example/demo/api/resource/UsuarioResource.java
+++ src/main/java/com/example/demo/api/resource/UsuarioResource.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.api.dto.UsuarioDTO;
+import com.example.demo.exception.ErroAutenticacao;
 import com.example.demo.exception.RegraNegocioException;
 import com.example.demo.model.entity.Usuario;
 import com.example.demo.service.UsuarioService;
@@ -22,6 +23,16 @@ public class UsuarioResource {
 
   public UsuarioResource(UsuarioService service) {
     this.service = service;
+  }
+  
+  @PostMapping("/autenticar")
+  public ResponseEntity autenticar(@RequestBody UsuarioDTO dto) {
+    try {
+      Usuario usuarioAutenticado = service.autenticar(dto.getEmail(), dto.getSenha());
+      return ResponseEntity.ok(usuarioAutenticado);
+    }catch (ErroAutenticacao e) {
+      return ResponseEntity.badRequest().body(e.getMessage());
+    }
   }
   
   @PostMapping
