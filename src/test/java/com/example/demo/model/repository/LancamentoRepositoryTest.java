af7e1b25e8200ef3b6b4b2358b1a85778e8ea870
George Salu <george.salu10@gmail.com>
2020-03-02 12:49:47 -0300

    testes de integracao do repository de lancamentos

---
+++ src/test/java/com/example/demo/model/repository/LancamentoRepositoryTest.java
@@ -0,0 +1,24 @@
+package com.example.demo.model.repository;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@ActiveProfiles("test")
+public class LancamentoRepositoryTest {
+
+  @Autowired
+  LancamentoRepository repository;
+  
+  @Autowired
+  TestEntityManager entityManager;
+  
+}
