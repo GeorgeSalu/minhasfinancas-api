fe4ddb92ec57a475b6153b5978dac9250806a4ba
George Salu <george.salu10@gmail.com>
2020-03-02 11:38:27 -0300

    implementando o endpoint de alterar o status do lancamento

--- src/main/java/com/example/demo/api/resource/LancamentoResource.java
+++ src/main/java/com/example/demo/api/resource/LancamentoResource.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.api.dto.AtualizaStatusDTO;
 import com.example.demo.api.dto.LancamentoDTO;
 import com.example.demo.exception.RegraNegocioException;
 import com.example.demo.model.entity.Lancamento;
@@ -82,6 +83,27 @@ public class LancamentoResource {
       }
     }).orElseGet(() -> 
       new ResponseEntity("Lancamento nao encontrado na base de dados", HttpStatus.BAD_REQUEST));
+  }
+  
+  @PutMapping("{id}/atualizar-status")
+  public ResponseEntity atualizarStatus(@PathVariable("id") Long id, @RequestBody AtualizaStatusDTO dto) {
+    return service.obterPorId(id).map(entity -> {
+      StatusLancamento statusSelecionado = StatusLancamento.valueOf(dto.getStatus());
+      if(statusSelecionado == null) {
+        return ResponseEntity.badRequest().body("Nao foi possivel atualizar o status do lancamento");
+      }
+      
+      try {
+        
+        entity.setStatus(statusSelecionado);
+        service.atualizar(entity);
+        return ResponseEntity.ok(entity);
+      }catch (RegraNegocioException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+      }
+      
+    }).orElseGet(() -> 
+        new ResponseEntity("Lancamento nao entrontrado na base de dados", HttpStatus.BAD_REQUEST));
   }
   
   @DeleteMapping("{id}")
