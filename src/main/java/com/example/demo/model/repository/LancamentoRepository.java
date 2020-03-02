a68ae59ce3f03bd91d7487ef48ca8833aa4d8cfb
George Salu <george.salu10@gmail.com>
2020-03-02 12:09:46 -0300

    corrigindo jpql

--- src/main/java/com/example/demo/model/repository/LancamentoRepository.java
+++ src/main/java/com/example/demo/model/repository/LancamentoRepository.java
@@ -10,7 +10,7 @@ import com.example.demo.model.entity.Lancamento;
 
 public interface LancamentoRepository extends JpaRepository<Lancamento, Long>{
   
-  @Query(value = "select sum(l.valor) from Lancamanto l join l.usuario u "
+  @Query(value = "select sum(l.valor) from Lancamento l join l.usuario u "
       + "where u.id = :idUsuario and l.tipo =:tipo group by u")
   BigDecimal obterSaldoPorTipoLancamentoEUsuario(@Param("idUsuario") Long idUsuario,@Param("tipo") String tipo);
 
